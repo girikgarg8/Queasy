d6d128eb7f7c6dac76c4319087cf3db88bb1cb23
Girik Garg <girikgarg8@gmail.com>
2022-07-15 22:36:52 +0530

    First commit

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+    facebookAuth: {
+        clientID: '149050199145264',
+        clientSecret: '0483572fb4a327cca5f44e5524c3bee2',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    googleAuth: {
+        clientID: '315307412613-5r4n9e9pcdnp66shhqc4h2d5odnl9dkg.apps.googleusercontent.com',
+        clientSecret: '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',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    }
+    
+};
---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'db': 'mongodb://localhost/Queasy',
+    'jwtsecret': '97hw9a73hr2q@#$#@mo8afjoeidha0e8doh',
+    'username': '',
+    'pass': ''
+}
